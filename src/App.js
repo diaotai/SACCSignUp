9a0cad46d4fd8b8e0e54089af64498c4c2610618
diaotai <diaotai@diaotaideMacBook-Pro.local>
2017-03-06 21:25:39 +0800

    基本注册界面和数据库已完成

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Login from './components/Registration'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Login/>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Registration.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import '../styles/Login.css';
+
+const FormItem = Form.Item;
+
+class NormalLoginForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+    //    save(values.userName,values.password,values.phoneNumber)
+      }
+    });
+  }
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <FormItem>
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input addonBefore={<Icon type="user" />} placeholder="Username" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input addonBefore={<Icon type="lock" />} type="password" placeholder="Password" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('phoneNumber', {
+            rules: [{ required: true, message: 'Please input your phoneNumber!' }],
+          })(
+            <Input addonBefore={<Icon type="mobile" />} placeholder="phoneNumber" />
+          )}
+        </FormItem>
+        <FormItem>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Sign up
+          </Button>
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
+
+export default WrappedNormalLoginForm
